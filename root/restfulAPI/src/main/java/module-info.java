146de5f1e717176e43a779509813ec13bb2e7c82
munoabr8 <munoabr8@gmail.com>
2023-10-26 15:07:51 -0400

    prototype updated

--- root/restfulAPI/src/main/java/module-info.java
+++ root/restfulAPI/src/main/java/module-info.java
@@ -2,8 +2,12 @@ module restfulAPI {
 	exports financialData.iex;
  
 	requires transitive interfaceAdapters;
+	requires transitive spring.beans;
+	requires transitive spring.web;
+	requires spring.core;
 	
-
-	
-	
+	//  requires spring.web;
+	 //   requires spring.boot;
+	 //   requires spring.boot.autoconfigure;
+  	
 }

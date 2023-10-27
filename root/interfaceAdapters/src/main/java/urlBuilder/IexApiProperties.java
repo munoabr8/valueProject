5225b9a6cef2a4a9aa46de27eb098bf69ed80eea
munoabr8 <munoabr8@gmail.com>
2023-10-27 13:16:08 -0400

    update: spring boot + DI + Maven successful

--- root/interfaceAdapters/src/main/java/urlBuilder/IexApiProperties.java
+++ root/interfaceAdapters/src/main/java/urlBuilder/IexApiProperties.java
@@ -5,8 +5,13 @@ import org.springframework.stereotype.Component;
 @Component
 public class IexApiProperties {
 
+	
+	/*
+	 * Base URL: Use Standard URL https://api.iex.cloud/v1/ or Workspace-branded URL 
+	 * https://YOUR_WORKSPACE.iex.cloud/v1/
+	 */
 	public String getBaseUrl() {
-		// TODO Auto-generated method stub
+
 		return "https://api.iex.cloud/v1/";
 	}
 

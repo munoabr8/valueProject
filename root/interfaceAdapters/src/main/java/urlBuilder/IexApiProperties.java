f3535f25d6348952ce36ad3fa2877b7edf039769
munoabr8 <munoabr8@gmail.com>
2023-10-29 12:11:38 -0400

    Spring boot and tag/versioning automation 10/29/23

--- root/interfaceAdapters/src/main/java/urlBuilder/IexApiProperties.java
+++ root/interfaceAdapters/src/main/java/urlBuilder/IexApiProperties.java
@@ -4,6 +4,16 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class IexApiProperties {
+	/*
+	 * This class is specific to IEX financial
+	 * data. How can I modify this class so that
+	 * that I can be changed in the future if
+	 * needed?
+	 * 
+	 * FUNTION: Store properties of a certain
+	 * online data source.
+	 */
+	
 
 	
 	/*

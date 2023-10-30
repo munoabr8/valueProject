ade59d008459e181d999b60e77800921261b114c
munoabr8 <munoabr8@gmail.com>
2023-10-29 20:46:28 -0400

    Updated  local repo with new spring boot and hook capabilities.
    Build Successful.

---
+++ root/codemr/root/html/html_report_root/htmlx/modeldata/overview_metric_data.js
@@ -0,0 +1,1 @@
+var CodeMR_ProjectMetrics = {"Project Name":"root working set: {root}", "Number of Highly Problematic Classes":0, "Number of Entities":0, "Number of Problematic Classes":0, "Class Lines of Code":0, "Number of External Packages":0, "Number of Packages":0, "Number of External Entities":0}
---
+++ root/codemr/root/html/workingsets.js
@@ -0,0 +1,3 @@
+var EQ_workingSetList = [
+{name: 'root', path:'root'}
+];
